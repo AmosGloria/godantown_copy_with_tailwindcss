d90a952cfcd8e9b0d2ac9e315a551d4d88071fd5
unknown <amoskesegloria@gmail.com>
2025-02-23 19:24:15 +0100

    Fix:Modified the opportunities directory

--- app/opportunities/page.tsx
+++ app/opportunities/page.tsx
@@ -5,7 +5,7 @@ export default function VirtualCardFeature() {
     return (
         <div className="virtual-card-feature flex justify-between items-center font-sans h-screen overflow-hidden mx-[6%] px-[3%] mt-[2%]">
             <div className="w-1/2 p-5 text-left">
-                <h1 className="text-2xl text-black mt-[10%] mb-5 font-impact">
+                <h1 className="text-4xl text-black font-black mt-[10%] mb-5 font-impact">
                     Endless Possibilities <span className="block">with our Virtual Card</span> feature
                 </h1>
                 <p className="text-base text-gray-600">
